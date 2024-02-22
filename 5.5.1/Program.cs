22ac3287b023c00b4efc35a943550777395f3ca4
Ekaterina <rfvb.foto@mail.ru>
2024-02-22 12:20:35 +0200

    Добавьте файлы проекта.

---
+++ 5.5.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string text = Console.ReadLine();
+            SplitText(text);
+            EnterText(SplitText(text));
+            
+        }
+        static string[] SplitText(string text){
+           return text.Split();
+           
+        }
+        static void EnterText(string[] split)
+        {
+            foreach (string s in split)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+
+        }
+        
+        
+        
+
+    }
+    
+    
+}
---
+++ 5.5.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string text = Console.ReadLine();
+            Console.WriteLine(ReversWords(text));
+            Console.ReadKey();
+        }
+        static string ReversWords(string text)
+        {
+            string[] WordArray = SplitText(text);
+            string ResultString = "";
+            foreach (string NewString in WordArray.Reverse())
+            {
+                ResultString += NewString + " ";
+            }
+            return ResultString;
+
+        }
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+
+    }
+}
